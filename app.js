86c1c592c8f2d583874017c3101f3c3702bea862
udan-insith <udaninsith23613@gmail.com>
2026-03-09 18:07:52 +0530

    working

--- app.js
+++ app.js
@@ -7,13 +7,43 @@ const showMoreButton = document.getElementById("show-more-button")
 let inputData = ""
 let page = 1
 
-function searchImages() {
+async function searchImages() {
     inputData = searchInputEl.value
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
-    console.log(url)
+    const response = await fetch(url)
+    const data = await response.json()
+    if(page === 1) {
+        searchResultsEl.innerHTML = ""
+    }
+
+    const results = data.results
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div")
+        imageWrapper.classList.add("search-result")
+        const image = document.createElement("img")
+        image.src = result.urls.small
+        image.alt = result.alt_description
+        const imageLink = document.createElement("a")
+        imageLink.href = result.links.html
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description
+
+        imageWrapper.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+
+        searchResultsEl.appendChild(imageWrapper)
+    }) 
+
+    page++
+
+    if (page > 1) {
+        showMoreButton.style.display = "block"
+    } 
 }
 
 formEl.addEventListener("submit", (event) => {
     event.preventDefault()
+    page = 1
     searchImages()
 })
